60727624d7cf6e374fb7a6df2490cd62ce5386a5
gosho-kazuya <ketsume0211@gmail.com>
2020-01-03 22:56:12 +0900

    refactored

--- main.js
+++ main.js
@@ -1,41 +1,58 @@
-const INDEX_PAGE_URL = 'https://www.e4628.jp/?module=timesheet&action=browse'
-const EDIT_PAGE_URL = 'https://www.e4628.jp/'
-
-function goToIndex() {
-  location.href = INDEX_PAGE_URL
+const pages = {
+  LIST: 'LIST',
+  INPUT: 'INPUT',
+  OTHER: 'OTHER',
 }
 
-function navigateToInputPage(index = 0) {
-  const trs = document.querySelectorAll('tr.bgcolor_white[id*=fix]')
-  const tr = trs[index]
-  if (!tr) {
-    return
-  }
-  if (tr.innerText.includes('Core Time')) {
-    navigateToInputPage(index + 1)
-    return
-  }
-  if (!tr.innerText.includes('Core Time')) {
-    const button = tr.getElementsByTagName('input')[0]
-    button.click()
+function getCurrentPage() {
+  if (location.href.includes('module=timesheet&action=browse')) {
+    return pages.LIST
+  } else if (location.href === 'https://www.e4628.jp/') {
+    return pages.INPUT
+  } else {
+    return pages.OTHER
   }
 }
 
-function createSlot() {
-  document.querySelector('[name=hour1]').selectedIndex = 9
-  document.querySelector('[name=hour2]').selectedIndex = 19
-  document.querySelector('[name=minute1]').selectedIndex = 31
-  document.querySelector('[name=minute2]').selectedIndex = 1
-  document.querySelector('[alt=保存]').click()
+const actions = {
+  navigateToInputPage: (index = 0) => {
+    const tr = document.querySelectorAll('tr.bgcolor_white[id*=fix]')[index]
+
+    // No input field, maybe not target page
+    if (!tr) {
+      return
+    }
+
+    // "Core Time" means the date is already filled
+    if (!tr.innerText.includes('Core Time')) {
+      const button = tr.getElementsByTagName('input')[0]
+      button.click()
+      return
+    }
+
+    // Move to next day
+    actions.navigateToInputPage(index + 1)
+  },
+  saveWorkingHours: () => {
+    document.querySelector('[name=hour1]').selectedIndex = 9
+    document.querySelector('[name=hour2]').selectedIndex = 19
+    document.querySelector('[name=minute1]').selectedIndex = 31
+    document.querySelector('[name=minute2]').selectedIndex = 1
+    document.querySelector('[alt=保存]').click()
+  },
+  done: () => {
+    alert('input done')
+  },
 }
 
 function main() {
-  if (location.href.includes('module=timesheet&action=browse')) {
-    navigateToInputPage()
-  } else if (location.href === EDIT_PAGE_URL) {
-    createSlot()
-  } else {
-    alert('input done')
+  switch (getCurrentPage()) {
+    case pages.LIST:
+      return actions.navigateToInputPage()
+    case pages.INPUT:
+      return actions.saveWorkingHours()
+    case pages.OTHER:
+      return actions.done()
   }
 }
 
